4c91092c5eea89571ffc78960143640acf47e947
Wei Jie Chew <weijie.chew@gmail.com>
2025-11-12 22:19:34 +0800

    login, logout and registering functionality added

--- auroramart/users/views.py
+++ auroramart/users/views.py
@@ -1,3 +1,55 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from .models import User, Customer
+from .forms import CustomerRegistrationForm, CustomerLoginForm
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
+def user_login(request):
+    if request.user.is_authenticated:
+        return redirect('storefront:home')
+    
+    if request.method == 'POST':
+        form = CustomerLoginForm(request, data=request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                login(request, user)
+                messages.success(request, f'Welcome back, {user.first_name}!')
+                next_url = request.GET.get('next', 'storefront:home')
+                return redirect(next_url)
+    else:
+        form = CustomerLoginForm()
+    
+    return render(request, 'users/login.html', {
+        'form': form
+        })
+
+
+@login_required
+def user_logout(request):
+    logout(request)
+    messages.success(request, 'You have been logged out successfully.')
+    return redirect('storefront:home')
+
+def register(request):
+    if request.user.is_authenticated:
+        return redirect('storefront:home')
+    
+    if request.method == 'POST':
+        form = CustomerRegistrationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            messages.success(request, 'Account created successfully! Welcome to AuroraMart.')
+            return redirect('storefront:home')
+    else:
+        form = CustomerRegistrationForm()
+    
+    return render(request, 'users/register.html', {
+        'form': form
+        })
+
