4a818d2936e732226d699ca0efb250f8c3a0e915
Wei Jie Chew <weijie.chew@gmail.com>
2025-11-12 16:57:47 +0800

    products listed on index page and category page

--- auroramart/storefront/urls.py
+++ auroramart/storefront/urls.py
@@ -5,9 +5,9 @@ app_name = 'storefront'
 
 urlpatterns = [
     path('', views.index, name='home'),
-    path('products/', views.index, name='products'),
-    path('category/<slug:category>/', views.index, name='category'),
+    path('products/', views.products, name='products'),
+    path('products/<str:sku>/', views.index, name='product_detail'),
+    path('category/<str:category>/', views.category, name='category'),
     path('cart/', views.index, name='cart'),
     path('watchlist/', views.index, name='watchlist'),
-    path('products/<str:sku>/', views.index, name='product_detail'),
 ]
