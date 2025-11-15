b97370c78eb4263bae3dece6cca31aab917ad0e3
Wei Jie Chew <weijie.chew@gmail.com>
2025-11-15 12:08:17 +0800

    added recommendations to product page and fallback mechanism if no recommended products

--- auroramart/mlservices/get_recommendations.py
+++ auroramart/mlservices/get_recommendations.py
@@ -1,6 +1,7 @@
 import joblib
 from django.apps import apps
 from .predict_preferred_category import APP_PATH
+from storefront.models import Product
 
 def load_recommendations_rules():
     model_path = APP_PATH / 'mlmodels' / 'b2c_products_500_transactions_50k.joblib'
@@ -28,3 +29,20 @@ def get_recommendations(items, metric='confidence', top_n=5, loaded_rules=_PRODU
     recommendations.difference_update(items)
     
     return list(recommendations)[:top_n]
+
+def get_product_recommendations(product_skus, top_n=5):
+    recommendations = get_recommendations(items=product_skus, metric='lift', top_n=top_n)
+    print(recommendations)
+
+    if recommendations:
+        return Product.objects.filter(sku__in=recommendations, is_active=True, archived=False)
+    else:
+        print("No recommendations found from ML model, falling back to popular products in same categories.")
+        # get top n popular products from categories of input products
+        input_products = Product.objects \
+            .filter(sku__in=product_skus, is_active=True)
+        input_categories = input_products.values_list('category', flat=True) \
+            .distinct()
+        return Product.objects \
+            .filter(category__in=input_categories, is_active=True, archived=False) \
+            .order_by('-rating')[:top_n]
