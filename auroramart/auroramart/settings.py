0cdffd76ee720060855123cc3a6ec19e53f91634
Wei Jie Chew <weijie.chew@gmail.com>
2025-12-06 19:51:26 +0800

    markdown rendering in admin panel

--- auroramart/auroramart/settings.py
+++ auroramart/auroramart/settings.py
@@ -48,6 +48,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'core',
     'users',
     'storefront.apps.StorefrontConfig',
     'admin_panel',
